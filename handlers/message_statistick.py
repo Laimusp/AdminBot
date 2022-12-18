40f10fa07e835cfc466679eb85b8f829248f28d3
koban <armis3005@gmail.com>
2022-12-18 15:47:33 +0300

    Добавил статистику сообщений

---
+++ handlers/message_statistick.py
@@ -0,0 +1,32 @@
+from sqlite3 import Connection
+from datetime import datetime
+
+from aiogram import Router
+from aiogram.types import Message
+from aiogram.filters import Text
+from aiogram import F
+
+router = Router()
+
+
+@router.message(Text(startswith='Stat', ignore_case=True))
+async def get_chat_stat_handler(msg: Message, stat_connect: Connection):
+    cur = stat_connect.cursor()
+    date = datetime.strftime(datetime.now().date(), '%d.%m.%Y')
+    chat_stat = cur.execute("SELECT count FROM info WHERE chat_id = ? AND date = ?",
+                            (msg.chat.id, date)).fetchone() or (0,)
+    await msg.answer(f'<b>Количество сообщений за сегодня: <u>{chat_stat[0]}</u></b>')
+
+
+@router.message(F.content_type.in_({'text', 'audio', 'animation', 'sticker', 'video', 'voice', 'video_note'}))
+async def update_stat_handler(msg: Message, stat_connect: Connection):
+    cur = stat_connect.cursor()
+    date = datetime.strftime(datetime.now().date(), '%d.%m.%Y')
+    chat_stat = cur.execute("SELECT count FROM info WHERE chat_id = ? AND date = ?",
+                            (msg.chat.id, date)).fetchone() or (0,)
+    with stat_connect:
+        if chat_stat[0] == 0:
+            cur.execute("INSERT INTO info VALUES(?,?,?)", (msg.chat.id, date, 1))
+        else:
+            cur.execute("UPDATE info SET count = ? WHERE chat_id = ? AND date = ?",
+                        (chat_stat[0] + 1, msg.chat.id, date))
--- main.py
+++ main.py
@@ -4,12 +4,13 @@ import logging
 
 from aiogram import Bot, Dispatcher
 
-from handlers import ban, kick, ro_warn
+from handlers import ban, kick, ro_warn, message_statistick
 
 
 async def main():
     logging.basicConfig(level=logging.INFO)
     ro_connect = sqlite3.connect('database/ro_info.sqlite')
+    stat_connect = sqlite3.connect('database/stat.sqlite')
 
     bot = Bot(token='1799127573:AAEGlmHuLjreW5rgRWE_eC9dGCQ1V5ubX7Y', parse_mode='HTML')
     dp = Dispatcher()
@@ -17,10 +18,11 @@ async def main():
     dp.include_router(ban.router)
     dp.include_router(kick.router)
     dp.include_router(ro_warn.router)
+    dp.include_router(message_statistick.router)
 
     # пропускаем все накопленные входящие
     await bot.delete_webhook(drop_pending_updates=True)
-    await dp.start_polling(bot, ro_connect=ro_connect)
+    await dp.start_polling(bot, ro_connect=ro_connect, stat_connect=stat_connect)
 
 
 if __name__ == '__main__':
